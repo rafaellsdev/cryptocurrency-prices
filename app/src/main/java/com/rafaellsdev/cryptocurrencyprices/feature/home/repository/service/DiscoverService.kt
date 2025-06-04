4df0b0590dcf8498d8e0d973a7f07260d0a999de
Rafael de Lima Silva <rafael.lsdeveloper@gmail.com>
2025-06-04 20:59:37 +0100

    Fix chart API service imports and ensure newline endings

--- app/src/main/java/com/rafaellsdev/cryptocurrencyprices/feature/home/repository/service/DiscoverService.kt
+++ app/src/main/java/com/rafaellsdev/cryptocurrencyprices/feature/home/repository/service/DiscoverService.kt
@@ -2,7 +2,9 @@ package com.rafaellsdev.cryptocurrencyprices.feature.home.repository.service
 
 import com.rafaellsdev.cryptocurrencyprices.commons.const.URLs.CURRENCIES_SERVICE
 import com.rafaellsdev.cryptocurrencyprices.feature.home.repository.model.CurrencyResponse
+import com.rafaellsdev.cryptocurrencyprices.feature.home.repository.model.MarketChartResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface DiscoverService {
