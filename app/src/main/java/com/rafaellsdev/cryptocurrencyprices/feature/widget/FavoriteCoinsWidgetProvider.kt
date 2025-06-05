fd39535078f8cfbb844fe007750f37e3e9469038
Rafael de Lima Silva <rafael.lsdeveloper@gmail.com>
2025-06-05 01:22:30 +0100

    Fix widget data loading

--- app/src/main/java/com/rafaellsdev/cryptocurrencyprices/feature/widget/FavoriteCoinsWidgetProvider.kt
+++ app/src/main/java/com/rafaellsdev/cryptocurrencyprices/feature/widget/FavoriteCoinsWidgetProvider.kt
@@ -13,11 +13,15 @@ class FavoriteCoinsWidgetProvider : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         for (widgetId in appWidgetIds) {
-            val intent = Intent(context, FavoriteCoinsWidgetService::class.java)
+            val intent = Intent(context, FavoriteCoinsWidgetService::class.java).apply {
+                putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+                data = android.net.Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
+            }
             val views = RemoteViews(context.packageName, R.layout.widget_favorite_coins)
             views.setRemoteAdapter(R.id.widget_list, intent)
             views.setEmptyView(R.id.widget_list, R.id.widget_empty)
             appWidgetManager.updateAppWidget(widgetId, views)
+            appWidgetManager.notifyAppWidgetViewDataChanged(widgetId, R.id.widget_list)
         }
     }
 }
